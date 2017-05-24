614c2af0555f11255771b6424a2e1d97de4628b8
Tang <e0011840@u.nus.edu>
2017-05-24 13:36:22 +0800

    isValidNumber

--- src/ExpressionHelper.java
+++ src/ExpressionHelper.java
@@ -1,51 +1,56 @@
 
 public class ExpressionHelper {
 
-	public static boolean isValidNumber(String numberExpression) {
-		return true;
-	}
-	
-	public static boolean isValidVariable(String variableExpression) {
-		return true;
-	}
-	
-	public static boolean isValidString(String stringExpression) {
-		return true;
-	}
-	
-	public static boolean isValidNull(String nullExpression) {
-		return true;
-	}
-	
-	public static boolean isValidComparator(String comparatorExpression) {
-		return true;
-	}
-	
-	public static String[] ParseSingle(String expression) {
-		return new String[1];
-	}
-	
-	public static boolean standardize(String basicExpression) {
-		return true;
-	}
-	
-	public static boolean isValidBasicExpression(String basicExpression) {
-		return true;
-	}
-	
-	public static String simplify(String basicExpession) {
-		return "";
-	}
-	
-	public static String evaluate(String expression) {
-		return "";
-	}
-	
-	public static ExpressionDescription Parse(String fullExpression) {
-		return null;
-	}
-	
-	public static boolean isTautology(String fullExpression) {
-		return true;
-	}
+    public static boolean isValidNumber(String numberExpression) {
+        return numberExpression.matches("-?\\d+(\\.\\d+)?");
+
+    }
+
+    public static boolean isValidVariable(String variableExpression) {
+        return true;
+    }
+
+    public static boolean isValidString(String stringExpression) {
+        return true;
+    }
+
+    public static boolean isValidNull(String nullExpression) {
+        return true;
+    }
+
+    public static boolean isValidComparator(String comparatorExpression) {
+        return true;
+    }
+
+    public static String[] ParseSingle(String expression) {
+        return new String[1];
+    }
+
+    public static boolean standardize(String basicExpression) {
+        return true;
+    }
+
+    public static boolean isValidBasicExpression(String basicExpression) {
+        return true;
+    }
+
+    public static String simplify(String basicExpession) {
+        return "";
+    }
+
+    public static String evaluate(String expression) {
+        return "";
+    }
+
+    public static ExpressionDescription Parse(String fullExpression) {
+        return null;
+    }
+
+    public static boolean isTautology(String fullExpression) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValidNumber("-1.23"));
+    }
 }
