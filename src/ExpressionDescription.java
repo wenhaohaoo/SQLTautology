5986d5c59886189a4dd58a521b7a6d89e6e42905
Wen Hao Lau <whlau@uwaterloo.ca>
2017-05-24 12:48:05 +0800

    skeleton added to ExpressionDescription

--- src/ExpressionDescription.java
+++ src/ExpressionDescription.java
@@ -1,4 +1,14 @@
 
 public class ExpressionDescription {
 
+	String leftExpression, rightExpression, comparatorString, originalDescription;
+	boolean isSuccessful;
+	
+	public ExpressionDescription(String input) {
+		// TODO Auto-generated constructor stub
+		this.originalDescription = input;
+		// verification code
+		isSuccessful = true;
+	}
+	
 }
