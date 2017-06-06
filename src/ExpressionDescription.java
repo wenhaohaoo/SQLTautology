8656fb3303aa814a482bec67f922fdb95f72236e
Tang <e0011840@u.nus.edu>
2017-06-06 15:24:48 +0800

    parse method

--- src/ExpressionDescription.java
+++ src/ExpressionDescription.java
@@ -1,14 +1,48 @@
-
 public class ExpressionDescription {
 
-	String leftExpression, rightExpression, comparatorString, originalDescription;
-	boolean isSuccessful;
-	
-	public ExpressionDescription(String input) {
-		// TODO Auto-generated constructor stub
-		this.originalDescription = input;
-		// verification code
-		isSuccessful = true;
-	}
-	
+    private String leftExpression;
+    private String rightExpression;
+    private String comparatorString;
+    private String originalDescription;
+    boolean isSuccessful;
+
+    public ExpressionDescription(String input) {
+        // TODO Auto-generated constructor stub
+        this.setOriginalDescription(input);
+        // verification code
+        isSuccessful = true;
+    }
+
+    String getLeftExpression() {
+        return this.leftExpression;
+    }
+
+    void setLeftExpression(String expression) {
+        this.leftExpression = expression;
+    }
+
+    String getRightExpression() {
+        return this.rightExpression;
+    }
+
+    void setRightExpression(String rightExpression) {
+        this.rightExpression = rightExpression;
+    }
+
+    public String getComparatorString() {
+        return this.comparatorString;
+    }
+
+    public void setComparatorString(String comparatorString) {
+        this.comparatorString = comparatorString;
+    }
+
+    public String getOriginalDescription() {
+        return this.originalDescription;
+    }
+
+    public void setOriginalDescription(String originalDescription) {
+        this.originalDescription = originalDescription;
+    }
+
 }
